9169279c055363f878c77fbde044e86eb458ead0
sharhan-alhassan <sharhanalhassan@gmail.com>
2021-03-04 22:41:46 +0000

    edited the README.md

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,12 +8,12 @@ from . models import Question
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    # output = ', '.join([q.question_text for q in latest_question_list])
+    output = ', '.join([q.question_text for q in latest_question_list])
     template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(context)
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
